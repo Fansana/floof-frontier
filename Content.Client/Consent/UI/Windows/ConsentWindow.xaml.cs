ccac412026ef1f93ea0e9e76e0833997bd7a479a
LucasTheDrgn <kirbyfan.95@gmail.com>
2025-02-19 10:19:12 -0500

    Vore Fixes and Consent Menu Improvement (#568)
    * vore fixes and consent menu improvement
    
    * consent menu fixes
    
    * Update Content.Shared/Consent/ConsentTogglePrototype.cs
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
    
    * Category has a default of "" to pull uncategorized consents to the top (NoClone has been uncategorized as an example)
    ConsentTogglePrototype.CompareTo override modifier removed, does not compile with it
    System.Linq.Enumerable.ToList<T>() used instead of List constructor
    
    * I Understand Now
    
    ---------
    
    Co-authored-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>

--- Content.Client/Consent/UI/Windows/ConsentWindow.xaml.cs
+++ Content.Client/Consent/UI/Windows/ConsentWindow.xaml.cs
@@ -7,6 +7,7 @@ using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
+using System.Linq;
 
 namespace Content.Client.Consent.UI.Windows;
 
@@ -149,7 +150,9 @@ public sealed partial class ConsentWindow : FancyWindow
             ConsentList.RemoveAllChildren();
         _entries.Clear();
 
-        var consentprototypelist = _protoManager.EnumeratePrototypes<ConsentTogglePrototype>();
+        var consentprototypelist = _protoManager.EnumeratePrototypes<ConsentTogglePrototype>().ToList();
+        consentprototypelist.Sort();
+
         foreach (var prototype in consentprototypelist)
             AddConsentEntry(prototype);
 
