a7c09b288cda04f2b4392aedca390ed5aa4f17be
fenndragon <fenndragon@gmail.com>
2025-03-16 18:34:49 -0600

    Revert "Update BlindHealingComponent.cs"
    This reverts commit 650787b30388e0dfa4d00c90acffd2274d831986.

--- Content.Server/_EinsteinEngines/Silicon/BlindHealing/BlindHealingComponent.cs
+++ Content.Server/_EinsteinEngines/Silicon/BlindHealing/BlindHealingComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Server._EinsteinEngines.Silicon.BlindHealing;
 [RegisterComponent]
 public sealed partial class BlindHealingComponent : Component
 {
-    public string SolutionName = "BlindHealing";
+    public string SolutionName = "BatteryDrinkerSource";
     
     [DataField]
     public int DoAfterDelay = 3;
